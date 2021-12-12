978cd444a5fe8e6836a29c0ee103e7a7f06d3826
rishabh1208 <rishabhsinghal1215@gmail.com>
2021-12-11 22:27:55 -0800

    added

---
+++ index.php
@@ -0,0 +1,15 @@
+<? ob_start() ?>
+<?
+# redirect to other page
+header("Location: https://rs1215.me/MarketPlace/login.php");
+?>
+<!-- <!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Meta Tag</title>
+      <meta http-equiv = "refresh" content = "2; url = https://rs1215.me/MarketPlace/login.php" />
+   </head>
+   <body>
+      <p>Common MarketPlace!</p>
+   </body>
+</html> -->
